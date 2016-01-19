a55c67f487b1037b73a6792b0da431ef12eee70b
user <495871401>
2016-01-19 18:31:23 +0800

    Initial Commit

---
+++ 算法/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  算法
+//
+//  Created by user on 16/1/19.
+//  Copyright © 2016年 mredrock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
